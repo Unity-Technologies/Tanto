ed088596e3479d82452ea907e0d6c9eecaf33621
Kenneth Skovhus <kenneth.skovhus@gmail.com>
2016-12-05 10:44:03 +0100

    Speed up dev build from 19 to 15 seconds

--- webpack/config.dev.js
+++ webpack/config.dev.js
@@ -9,7 +9,7 @@ const port = (+process.env.PORT + 1) || 3001
 const baseConfig = require('./config.base')
 
 module.exports = Object.assign({}, baseConfig.config, {
-  devtool: 'inline-source-map',
+  devtool: 'eval',
   entry: {
     main: [
       `webpack-hot-middleware/client?path=http://${host}:${port}/__webpack_hmr`,
