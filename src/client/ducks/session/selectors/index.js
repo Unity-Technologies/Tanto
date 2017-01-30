12b7eeca5481d8352335fc5f46722f7777624456
Kateryna Musina <kateryna@unity3d.com>
2017-01-30 15:09:27 +0100

    Fixed session selectors on handling empty pull requests list

--- src/client/ducks/session/selectors/index.js
+++ src/client/ducks/session/selectors/index.js
@@ -14,18 +14,21 @@ export const pullRequestsSelector = state => state.pullrequests.entities
 export const pullRequestsOwned = createSelector(
   pullRequestsSelector, pullRequestsOwnedIdsSelector,
   (pullRequests, pullRequestsOwnedIds) =>
-    _.values(_.pick(pullRequests, pullRequestsOwnedIds))
+    (_.isEmpty(pullRequests) || !pullRequestsOwnedIds ? [] :
+    _.values(_.pick(pullRequests, pullRequestsOwnedIds)))
 )
 
 export const pullRequestsAssigned = createSelector(
   pullRequestsSelector, pullRequestsAssignedIdsSelector,
   (pullRequests, pullRequestsAssignedIds) =>
-    _.values(_.pick(pullRequests, pullRequestsAssignedIds))
+    (_.isEmpty(pullRequests) || !pullRequestsAssignedIds ? [] :
+    _.values(_.pick(pullRequests, pullRequestsAssignedIds)))
 )
 
 export const pullRequestsWatching = createSelector(
   pullRequestsSelector, pullRequestsWatchingIdsSelector,
   (pullRequests, pullRequestsWatchingIds) =>
-    _.values(_.pick(pullRequests, pullRequestsWatchingIds))
+    (_.isEmpty(pullRequests) ? [] :
+    _.values(_.pick(pullRequests, pullRequestsWatchingIds)))
 )
 
