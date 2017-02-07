c6d18768d782deb81e1deef7b1ca141aad32a33c
Katarzyna Tumidajewicz <katarzyna@unity3d.com>
2017-02-07 15:44:02 +0100

    Fix for "<Link>s rendered outside of a router context cannot navigate." error in Link story (possibly other stories using links as well)

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,26 +1,13 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
-// This is just the basic way to add addional webpack configurations.
-// For more information refer the docs: https://goo.gl/qPbSyX
+// load the default config generator.
+const genDefaultConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js')
 
-// IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
-// to "React Create App". This only has babel loader to load JavaScript.
+module.exports = function genConfig(config, env) {
+  // You can use your own config here as well, instead our default config.
+  const newConfig = genDefaultConfig(config, env)
 
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: 'style!css?localIdentName=[local]___[hash:base64:5]',
-      },
-      {
-        test: /\.png$/,
-        loader: 'url-loader',
-        query: { mimetype: 'image/png' },
-      },
-    ],
-  },
+  // this is used by our custome `rr.js` module
+  newConfig.resolve.alias['react-router-original'] = require.resolve('react-router')
+  // this `rr.js` will replace the Link with a our own mock component.
+  newConfig.resolve.alias['react-router'] = require.resolve('./rr.js')
+  return newConfig
 }
