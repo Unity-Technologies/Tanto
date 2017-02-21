f44254df2437e342add1c101b2311bb21698ffbf
Katarzyna Tumidajewicz <katarzyna@unity3d.com>
2017-02-21 15:57:02 +0100

    added second test for getData selector

--- src/client/containers/PullRequestCommits/__tests__/index.js
+++ src/client/containers/PullRequestCommits/__tests__/index.js
@@ -6,8 +6,8 @@ import { types } from 'ducks/pullrequests/actions'
 
 const expect = chai.expect
 
-describe('data selectors', () => {
-  it('getData returns something', () => {
+describe('getData', () => {
+  it('with one pr, using [props.params.prid]', () => {
     const changeset = 'changeset'
     const pullRequest = {
       id: 12,
@@ -35,6 +35,29 @@ describe('data selectors', () => {
     const props = {
       params: {
         prid: 12,
+      },
+      status,
+    }
+    expect(getData(state, props)(state, props)).to.deep.eql(data)
+  })
+  it('with no matching pr', () => {
+    const status = {
+      isFetching: false,
+      error: null,
+    }
+    const data = {
+      commits: [],
+      projectName: '',
+      status,
+    }
+    const key = types.FETCH_PULL_REQUEST_CHANGESET
+    const state = {
+      fetch: { [key]: {} },
+      entities: { pullRequests: { 12: 'pullRequest' } },
+    }
+    const props = {
+      params: {
+        prid: 13,
       },
       status,
     }
