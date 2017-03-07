1002bab2a5569ad24552154786b8b6ed6cd3f4f9
Katarzyna Tumidajewicz <katarzyna@unity3d.com>
2017-03-07 16:55:20 +0100

    added negative test scenarios for PullRequestSummary/RepositoriesSection getRepositoriesData selector

--- src/client/containers/PullRequestSummary/RepositoriesSection/__tests__/index.js
+++ src/client/containers/PullRequestSummary/RepositoriesSection/__tests__/index.js
@@ -5,8 +5,8 @@ import {
 
 const expect = chai.expect
 
-describe('getRepositoriesData', () => {
-  it('full data', () => {
+describe('PullRequestSummary/RepositoriesSection selectors', () => {
+  it('getRepositoriesData, full data', () => {
     const expectedData = {
       origin: 'origin',
       target: 'target',
@@ -21,6 +21,36 @@ describe('getRepositoriesData', () => {
       entities: {
         pullRequests,
       },
+    }
+    const props = {
+      params: {
+        prid: 12,
+      },
+    }
+    expect(getRepositoriesData(state, props)(state, props)).to.deep.eql(expectedData)
+  })
+  it('getRepositoriesData, no pr (no prid in props)', () => {
+    const expectedData = {}
+    const pullRequests = {
+      12: {
+        ...expectedData,
+        otherParameter: 'otherParameter',
+      },
+    }
+    const state = {
+      entities: {
+        pullRequests,
+      },
+    }
+    const props = {
+      params: {},
+    }
+    expect(getRepositoriesData(state, props)(state, props)).to.deep.eql(expectedData)
+  })
+  it('getRepositoriesData, no pr in state', () => {
+    const expectedData = {}
+    const state = {
+      entities: {},
     }
     const props = {
       params: {
