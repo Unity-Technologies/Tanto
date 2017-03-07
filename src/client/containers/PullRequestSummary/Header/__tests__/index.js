396ef755e665b6214f44eda6756a642f5d835d4e
Katarzyna Tumidajewicz <katarzyna@unity3d.com>
2017-03-07 16:34:25 +0100

    added negative test scenarios for PullRequestSummary/Header getHeaderData selector

--- src/client/containers/PullRequestSummary/Header/__tests__/index.js
+++ src/client/containers/PullRequestSummary/Header/__tests__/index.js
@@ -2,12 +2,11 @@ import chai from 'chai'
 import {
   getHeaderData,
 } from '../index'
-import { types } from 'ducks/pullrequests/actions'
 
 const expect = chai.expect
 
-describe('getHeaderData', () => {
-  it('full data', () => {
+describe('PullRequestSummary/Header selectors', () => {
+  it('getHeaderData, full data', () => {
     const user1 = { id: 1, username: 'testusername1' }
     const user3 = { id: 3, username: 'testusername3' }
     const users =
@@ -25,9 +24,78 @@ describe('getHeaderData', () => {
       12: pullRequestHeaderData,
     }
     const state = {
-      fetch: { [types.FETCH_PULL_REQUEST_ISSUES]: {} },
       entities: {
         pullRequests,
+        users,
+      },
+    }
+    const props = {
+      params: {
+        prid: 12,
+      },
+    }
+    expect(getHeaderData(state, props)(state, props)).to.deep.eql(expectedData)
+  })
+  it('getHeaderData, no users in state', () => {
+    const pullRequestHeaderData = {
+      title: 'someTitle',
+      created: '2017-02-27',
+      owner: 1,
+    }
+    const expectedData = { ...pullRequestHeaderData, owner: undefined }
+    const pullRequests = {
+      12: pullRequestHeaderData,
+    }
+    const state = {
+      entities: {
+        pullRequests,
+        users: {},
+      },
+    }
+    const props = {
+      params: {
+        prid: 12,
+      },
+    }
+    expect(getHeaderData(state, props)(state, props)).to.deep.eql(expectedData)
+  })
+  it('getHeaderData, no pr (no prid in props)', () => {
+    const user1 = { id: 1, username: 'testusername1' }
+    const user3 = { id: 3, username: 'testusername3' }
+    const users =
+      {
+        1: user1,
+        3: user3,
+      }
+    const pullRequestHeaderData = {
+      title: 'someTitle',
+      created: '2017-02-27',
+      owner: 1,
+    }
+    const expectedData = {}
+    const pullRequests = {
+      12: pullRequestHeaderData,
+    }
+    const state = {
+      entities: {
+        pullRequests,
+        users,
+      },
+    }
+    const props = {}
+    expect(getHeaderData(state, props)(state, props)).to.deep.eql(expectedData)
+  })
+  it('getHeaderData, no pr in state', () => {
+    const user1 = { id: 1, username: 'testusername1' }
+    const user3 = { id: 3, username: 'testusername3' }
+    const users =
+      {
+        1: user1,
+        3: user3,
+      }
+    const expectedData = {}
+    const state = {
+      entities: {
         users,
       },
     }
