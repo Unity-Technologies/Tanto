6eaf09d75d3dec06d1cd24bb2232e46e5b094fa4
Katarzyna Tumidajewicz <katarzyna@unity3d.com>
2017-03-14 14:00:27 +0100

    added unit tests for getPRequest in PullRequestDiscussion

--- src/client/containers/PullRequestDiscussion/__tests__/index.js
+++ src/client/containers/PullRequestDiscussion/__tests__/index.js
@@ -1,13 +1,12 @@
 import chai from 'chai'
 import {
-  getPullRequestDiscussion,
+  getPRequest,
 } from '../PullRequestDiscussion'
-import { types } from 'ducks/pullrequests/actions'
 
 const expect = chai.expect
 
-describe('getPullRequestDiscussion', () => {
-  it('full data', () => {
+describe('PullRequestDiscussion selectors', () => {
+  it('getPRequest, full data', () => {
     const user1 = { id: 1, username: 'testusername1' }
     const user3 = { id: 3, username: 'testusername3' }
     const users =
@@ -22,10 +21,11 @@ describe('getPullRequestDiscussion', () => {
         1: comment1,
         2: comment2,
       }
-    const expectedPullRequest = {
+    const expectedPullRequestData = {
       description: 'testdescription',
       created: '2017-02-28',
       owner: user1,
+      origin: 'origin',
       comments: [
           { ...comment1, author: user1 },
           { ...comment2, author: user3 },
@@ -33,40 +33,180 @@ describe('getPullRequestDiscussion', () => {
     }
     const pullRequests = {
       12: {
-        ...expectedPullRequest,
+        ...expectedPullRequestData,
         otherAttribute: 'otherAttribute',
         owner: 1,
         comments: [1, 2],
       },
     }
     const state = {
-      fetch: { [types.FETCH_PULL_REQUEST_DISCUSSION]: {} },
       entities: {
         pullRequests,
         users,
         comments,
       },
-      session: {
-        profile: {
-          fullName: 'userName',
-        },
-      },
-    }
-    const status = {
-      isFetching: false,
-      error: null,
     }
     const props = {
       params: {
         prid: 12,
       },
-      status,
     }
-    const expectedData = {
-      pullRequest: expectedPullRequest,
-      user: 'userName',
-      status,
+    expect(getPRequest(state, props)).to.deep.eql(expectedPullRequestData)
+  })
+  it('no users in state: getPRequest should return data without users', () => {
+    const comment1 = { id: 1, author: 1 }
+    const comment2 = { id: 2, author: 3 }
+    const comments =
+      {
+        1: comment1,
+        2: comment2,
+      }
+    const expectedPullRequestData = {
+      description: 'testdescription',
+      created: '2017-02-28',
+      owner: undefined,
+      origin: 'origin',
+      comments: [
+          { ...comment1, author: {} },
+          { ...comment2, author: {} },
+      ],
     }
-    expect(getPullRequestDiscussion(state, props)(state, props)).to.deep.eql(expectedData)
+    const pullRequests = {
+      12: {
+        ...expectedPullRequestData,
+        otherAttribute: 'otherAttribute',
+        owner: 1,
+        comments: [1, 2],
+      },
+    }
+    const state = {
+      entities: {
+        pullRequests,
+        comments,
+      },
+    }
+    const props = {
+      params: {
+        prid: 12,
+      },
+    }
+    expect(getPRequest(state, props)).to.deep.eql(expectedPullRequestData)
+  })
+  it('no PRs in state: getPRequest should return empty data', () => {
+    const user1 = { id: 1, username: 'testusername1' }
+    const user3 = { id: 3, username: 'testusername3' }
+    const users =
+      {
+        1: user1,
+        3: user3,
+      }
+    const comment1 = { id: 1, author: 1 }
+    const comment2 = { id: 2, author: 3 }
+    const comments =
+      {
+        1: comment1,
+        2: comment2,
+      }
+    const expectedPullRequestData = {
+      comments: [],
+    }
+    const state = {
+      entities: {
+        users,
+        comments,
+      },
+    }
+    const props = {
+      params: {
+        prid: 12,
+      },
+    }
+    expect(getPRequest(state, props)).to.deep.eql(expectedPullRequestData)
+  })
+  it('no comments in state: getPRequest should return data without comments', () => {
+    const user1 = { id: 1, username: 'testusername1' }
+    const user3 = { id: 3, username: 'testusername3' }
+    const users =
+      {
+        1: user1,
+        3: user3,
+      }
+    const expectedPullRequestData = {
+      description: 'testdescription',
+      created: '2017-02-28',
+      owner: user1,
+      origin: 'origin',
+      comments: [],
+    }
+    const pullRequests = {
+      12: {
+        ...expectedPullRequestData,
+        otherAttribute: 'otherAttribute',
+        owner: 1,
+        comments: [1, 2],
+      },
+    }
+    const state = {
+      entities: {
+        pullRequests,
+        users,
+      },
+    }
+    const props = {
+      params: {
+        prid: 12,
+      },
+    }
+    expect(getPRequest(state, props)).to.deep.eql(expectedPullRequestData)
+  })
+  it('no entities in state: getPRequest should return empty data', () => {
+    const expectedPullRequestData = {
+      comments: [],
+    }
+    const state = {
+      entities: {},
+    }
+    const props = {
+      params: {
+        prid: 12,
+      },
+    }
+    expect(getPRequest(state, props)).to.deep.eql(expectedPullRequestData)
+  })
+  it('no prid in props: getPRequest should return empty data', () => {
+    const user1 = { id: 1, username: 'testusername1' }
+    const user3 = { id: 3, username: 'testusername3' }
+    const users =
+      {
+        1: user1,
+        3: user3,
+      }
+    const comment1 = { id: 1, author: 1 }
+    const comment2 = { id: 2, author: 3 }
+    const comments =
+      {
+        1: comment1,
+        2: comment2,
+      }
+    const expectedPullRequestData = {
+      comments: [],
+    }
+    const pullRequests = {
+      12: {
+        ...expectedPullRequestData,
+        otherAttribute: 'otherAttribute',
+        owner: 1,
+        comments: [1, 2],
+      },
+    }
+    const state = {
+      entities: {
+        pullRequests,
+        users,
+        comments,
+      },
+    }
+    const props = { params: {} }
+    expect(getPRequest(state, props)).to.deep.eql(expectedPullRequestData)
   })
 })
