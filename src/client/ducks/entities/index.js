1495596baf65f6fe59fa99b2d03cd7219ece0e4c
Kateryna Musina <kateryna@unity3d.com>
2017-03-17 16:35:06 +0100

    Unit tests

--- src/client/ducks/entities/index.js
+++ src/client/ducks/entities/index.js
@@ -10,17 +10,18 @@ export const types = {
   SET_MUTATED_ENTITIES,
 }
 
-const queryCustomizer = (objValue: any, srcValue: any): any => { // eslint-disable-line consistent-return
+export const queryCustomizer = (objValue: any, srcValue: any): any => { // eslint-disable-line consistent-return
   if (_.isArray(objValue)) {
     return _.union(objValue, srcValue)
   }
 }
 
-const mutationCustomizer = (objValue: any, srcValue: any, key, object, source, stack): any => { // eslint-disable-line consistent-return
-  if (_.isArray(objValue)) {
-    return srcValue
+export const mutationCustomizer =
+  (objValue: any, srcValue: any, key, object, source, stack): any => { // eslint-disable-line consistent-return
+    if (_.isArray(objValue)) {
+      return srcValue
+    }
   }
-}
 
 export const merge = (state: Object, entity: Object, customizer: Function): Object => {
   const updatedState = _.mergeWith({}, state, entity, customizer)
