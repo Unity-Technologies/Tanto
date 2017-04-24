e3f2b4e8b80134ab2c0e1eb586ef8fba689dad3d
Henrik Stuart <henriks@unity3d.com>
2017-04-24 15:27:00 +0200

    redis: always use connection settings

--- src/server/redis/index.js
+++ src/server/redis/index.js
@@ -1,14 +1,21 @@
 /* eslint-disable no-console */
 import redis from 'redis'
+import env from '../config'
 
 let client
 
 export const handleError = (error) => console.error(error)
 
+const redisOptions = {
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  prefix: env.REDIS_SESSION_PREFIX,
+}
+
 export const redisClient = {
   client: () => {
     if (!client) {
-      client = redis.createClient()
+      client = redis.createClient(redisOptions)
       client.unref()
       client.on('error', handleError)
     }
